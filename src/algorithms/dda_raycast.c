73b329649e44faf6bf5c2d45e995ef0cde0a798e
hotmiamy <mrllps07@gmail.com>
2023-04-15 04:17:09 -0300

    fix: raycasting: var -> {wall_line_height[case(HEIGHT / 0,99)]}

--- src/algorithms/dda_raycast.c
+++ src/algorithms/dda_raycast.c
@@ -6,7 +6,7 @@
 /*   By: llopes-n <llopes-n@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/01 07:57:11 by tyago-ri          #+#    #+#             */
-/*   Updated: 2023/04/15 01:52:56 by llopes-n         ###   ########.fr       */
+/*   Updated: 2023/04/15 04:01:05 by llopes-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,10 +45,13 @@ void	raycasting(t_strc *strc, int pixel)
 	t_vector	point1;
 	t_vector	point2;
 
-	wall_line_height = HEIGHT / strc->dda.perpendicular;
-	point1.x = (double)pixel;
+	if (HEIGHT / strc->dda.perpendicular > HEIGHT)
+		wall_line_height = HEIGHT;
+	else
+		wall_line_height = HEIGHT / strc->dda.perpendicular;
+	point1.x = WIDTH - (double)pixel;
 	point1.y = HEIGHT / 2 - wall_line_height / 2;
-	point2.x = (double)pixel;
+	point2.x = WIDTH - (double)pixel;
 	point2.y = HEIGHT / 2 + wall_line_height / 2;
 	if (strc->dda.hit.side == 0)
 		bresenham(&point1, &point2, strc, get_rgb(255, 0, 0));
